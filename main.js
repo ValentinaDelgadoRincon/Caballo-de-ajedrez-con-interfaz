f32f6834e0293aee816b2e80b583817e222b0e18
CamilaFlorez12 <07camilaflorezsantos@gmail.com>
2025-06-05 08:57:32 -0500

    feat: :sparkles: adde js
    added js
    
    by@camilaflorez

--- main.js
+++ main.js
@@ -1,7 +1,16 @@
-const fila=document.querySelector("#fila");
-const columna=document.querySelector("#columna");
-const calcular=document.querySelector("#boton");
-if(fila!==0 && columna!==0){
-    fila=2
-    columna=1
+function calcular(){
+    const fila=document.getElementById("fila").value;
+    const columna=document.getElementById("columna").value;
+    const resultado=document.getElementById("boton").value;
+    if(fila>8 && columna>8){
+        alert("Numero no valido")
+    }
+    for(let filas=0;i<fila;i++){
+        filas+=2
+    }
+    for(let columnas;i<columna;i++){
+        columnas+=1
+    }
+    resultado=columna && fila
 }
+
