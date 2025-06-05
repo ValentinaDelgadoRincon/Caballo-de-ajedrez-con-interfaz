02696cb766e4bb188bfde12cc761bd828785a877
CamilaFlorez12 <07camilaflorezsantos@gmail.com>
2025-06-05 08:37:53 -0500

    feat: :sparkles: added css
    added css
    
    by@camilaflorez

--- main.js
+++ main.js
@@ -1,3 +1,7 @@
 const fila=document.querySelector("#fila");
 const columna=document.querySelector("#columna");
 const calcular=document.querySelector("#boton");
+if(fila!==0 && columna!==0){
+    fila=2
+    columna=1
+}
