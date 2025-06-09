36435c8ada6a3a76b3bc7a08d148e87aa80144eb
CamilaFlorez12 <07camilaflorezsantos@gmail.com>
2025-06-08 19:49:37 -0500

    fix: :bug: fixed js
    fixed js
    
    by@camilaflorez

--- main.js
+++ main.js
@@ -1,16 +1,35 @@
 function calcular(){
-    const fila=document.getElementById("fila").value;
-    const columna=document.getElementById("columna").value;
-    const resultado=document.getElementById("boton").value;
-    if(fila>8 && columna>8){
-        alert("Numero no valido")
+    const fila=parseInt(document.getElementById("fila").value);
+    const columna=parseInt(document.getElementById("columna").value);
+    const resultado=document.getElementById("resultado");
+    if(fila < 1||fila > 8 || columna < 1|| columna > 8){
+        alert("Numero no valido");
+        return;
     }
-    for(let filas=0;i<fila;i++){
-        filas+=2
+    const movimientos=[
+        [fila+2,columna+1],
+        [fila+2,columna-1],
+        [fila-2,columna+1],
+        [fila -2,columna-1],
+        [fila+1,columna+2],
+        [fila+1,columna-2],
+        [fila-1,columna+2],
+        [fila-1,columna-2],
+    ]
+    let validos=[];
+    for(let i=0;i<movimientos.length;i++){
+        let [filas,columnas]=movimientos[i];
+        if(filas>=1&& filas<=8 && columnas>=1 && columnas<=8){
+            validos.push([filas,columnas])
+        }
     }
-    for(let columnas;i<columna;i++){
-        columnas+=1
-    }
-    resultado.push(fila + columna);
-}
+    if(validos.length===0){
+        resultado.textContent="No es un movimiento válido";
+    }else{
+        let texmovimientos=validos
+        .map((posicion)=>`(${posicion[0]} ,${posicion[1]})`)
+        .join(" , ");
+        resultado.textContent=`El caballo en ${fila}, ${columna} puede moverse a:\n ${texmovimientos}`;
 
+    }
+}
